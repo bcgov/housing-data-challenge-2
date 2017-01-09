851b6ce9f19fdd5d9ce9dfea630de83d0eafcef6
exponentian.oh <exponentian.oh@gmail.com>
2017-01-09 00:42:17 -0800

    bootstrap order changed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap-sprockets
+
 //= require jquery
 //= require jquery_ujs
 //= require charts
 //= require leaflet
 //= require bc-devx
-
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
